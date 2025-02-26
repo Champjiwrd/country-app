17a204f1bd092e2a9ea579231dd50c3a3e9b8879
Warodom Baiduom <warodom@mm.co.th>
2025-02-27 00:51:38 +0700

    baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
   compatibilityDate: '2024-11-01',
-  router: {
-    base: '/country-app/', // Replace with your repository name
+  app: {
+    baseURL: '/country-app/',
   },
   devtools: { enabled: true },
   routeRules: {
