21f088eb12ed674c74b061ee93d3e9f49a7103e4
Warodom Baiduom <warodom@mm.co.th>
2025-02-27 00:58:00 +0700

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,22 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   app: {
     baseURL: '/country-app/',
+    head: {
+      title: 'เกมทายแผนที่ | เกมทดสอบความรู้ภูมิศาสตร์ของคุณ',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content:
+            'width=device-width, height=device-height, target-densitydpi=device-dpi, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no',
+        },
+        {
+          name: 'description',
+          content:
+            'ทายแผนที่ต่าง ๆ และทดสอบความรู้ภูมิศาสตร์ของคุณ! เกมสนุก ๆ ที่จะช่วยคุณจำแผนที่ได้ดียิ่งขึ้น',
+        },
+      ],
+    },
   },
   devtools: { enabled: true },
   routeRules: {
