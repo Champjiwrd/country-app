936e9a47b29f2a1388fbaeb1f791dc018c272231
Warodom Baiduom <warodom@mm.co.th>
2025-02-27 09:38:07 +0700

    title

---
+++ pages/dynamic/[name].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold">{{ name }}</h1>
+    <p>Welcome to the page about {{ name }}!</p>
+  </div>
+</template>
+<script setup>
+const route = useRoute();
+const name = route.params.name;
+
+useHead({
+  title: `title from ${name} [name] page`,
+  meta: [{ name: 'description', content: `description from ${name} [name] ` }],
+});
+</script>
+
+<style scoped>
+/* Add any custom styling here */
+</style>
---
+++ pages/public.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>public page</div>
+</template>
+
+<script setup>
+useHead({
+  title: `Title from public page`,
+  meta: [{ name: 'description', content: `description from public page` }],
+});
+</script>
+
+<style></style>
