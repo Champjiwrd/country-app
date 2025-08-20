6603b4186fbec7eb157d4466984736d5cad6ce09
Warodom Baiduom <warodom@mm.co.th>
2025-08-20 12:55:15 +0700

    feat(page): test page

---
+++ pages/dynamic/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold">{{ name }}</h1>
+    <p>Welcome to the page about {{ name }}!</p>
+  </div>
+</template>
+<script setup>
+const name = 'test'
+
+useHead({
+  title: name ? `title from ${name} [name] page` : 'title from [name] page but cannot use name',
+  meta: [{ name: 'description', content: name? `description from ${name} [name] ` : 'description from name but connot use name' }],
+});
+</script>
+
+<style scoped>
+/* Add any custom styling here */
+</style>
