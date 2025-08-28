cc71f7a4647960f673488844782c331c083fe67d
Warodom Baiduom <warodom@mm.co.th>
2025-08-28 09:56:43 +0700

    update seo

--- pages/dynamic/[[name]].vue
+++ pages/dynamic/[[name]].vue
@@ -7,19 +7,26 @@
 <script setup>
 const route = useRoute();
 const name = route.params.name;
-defineRouteRules({
-  prerender: true,
-});
-useHead({
-  title: name ? `title from ${name} [name] page` : 'title from [name] page but cannot use name',
-  meta: [{ name: 'description', content: name? `description from ${name} [name] ` : 'description from name but connot use name' }],
-});
+// defineRouteRules({
+//   prerender: true,
+// });
+// useHead({
+//   title: name ? `title from ${name} [name] page` : 'title from [name] page but cannot use name',
+//   meta: [{ name: 'description', content: name? `description from ${name} [name] ` : 'description from name but connot use name' }],
+// });
 // useSeoMeta({
 //   title: () => name ? `seo meta: title from ${name} [name] page` : 'title from [name] page but cannot use name',
 //   description: () => name? `seo meta: description from ${name} [name] ` : 'description from name but connot use name' ,
 //   ogTitle: () => name ? `seo meta: title from ${name} [name] page` : 'title from [name] page but cannot use name',
 //   ogDescription: () => name? `seo meta: og:description from ${name} [name] ` : 'description from name but connot use name' ,
 // });
+useSeoMeta({
+  title: `${name} แปลว่าอะไร ดูความหมาย ตัวอย่างประโยค หมายความว่า พจนานุกรม Longdo Dictionary แปลภาษา คำศัพท์`,
+  ogTitle: `คำศัพท์ ${name} แปลว่าอะไร`,
+  ogType: 'article',
+  // ogUrl: `${currentDictUrl}search/${encodeURIComponent(search.value)}`,
+  // ogDescription: 'ดูความหมายที่พจนานุกรมลองดู Longdo Dictionary',
+});
 </script>
 
 <style scoped>
